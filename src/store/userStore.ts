5c3cdef2362ff4f8957866f52e4c196feb2af0f1
prudhvi deep <prudhvi@clucoud.com>
2024-12-25 20:52:48 -0700

    Updated the build issue

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -4,12 +4,14 @@ import createSelectors from "./createSelectors"
 interface UserState {
   uid : string | null,
   email : string | null,
+  password : string | null,
   showMessage : boolean,
   authError : boolean,
   authErrorMessage : string | null,
   userName : string | null,
   setUid : (uid : string | null) => void;
   setEmail : (email : string | null) => void;
+  setPassword : (pass : string | null) => void;
   setAuthError : (error : boolean) => void;
   setShowMessage : (value : boolean) => void;
   setAuthErrorMessage : (errMessage : string | null) => void;
@@ -19,12 +21,14 @@ interface UserState {
 const useUserStoreBase = create<UserState>((set) => ({
   uid : null,
   email : null,
+  password : null,
   showMessage : false,
   authError : false,
   authErrorMessage : null,
   userName : null,
   setUid : (uid) => set({uid : uid}),
   setEmail : (email) => set({email : email}),
+  setPassword : (password) => set({password : password}),
   setAuthError : (error) => set({authError : error}),
   setShowMessage : (value) => set({showMessage : value}),
   setAuthErrorMessage : (errMessage) => set({authErrorMessage : errMessage}),
